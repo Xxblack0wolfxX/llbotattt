4541ef44b2a2ca4b50c7147fda1c2ef644ccdacb
Xxblack0wolfxX <54740430+Xxblack0wolfxX@users.noreply.github.com>
2019-09-26 16:18:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -331,7 +331,7 @@ client.on('message', message => {
     }
 });
 
-const developers = ["472413769700474901","id"]
+const developers = ["498452643845373962","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
